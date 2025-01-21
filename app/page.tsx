e7abb847b8fe2bbfc87c1ceed26355ce081808f2
Elliot Henriquez <elliothenriq@gmail.com>
2025-01-20 21:07:10 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ import MessageForm from "./contact/MessageForm"
 export default function Page() {
 	useEffect(() => {
 		// Set the date we're counting down to
-		var countDownDate = new Date("Jan 26, 2025 23:59:00").getTime();
+		var countDownDate = new Date("Feb 15, 2025 23:59:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
@@ -90,7 +90,7 @@ export default function Page() {
 				<h2 className='uppercase text-xl lg:text-4xl tracking-tighter font-bold text-orange-500'>founder's rate </h2>
 				<p className='text-xl lg:text-4xl tracking-tighter font-bold text-orange-500'>& NO registration fee! </p>
 				<p className='text-base lg:text-xl px-1'>Or call us at (281) 652-7961 for more information!</p>
-				<h2 className='uppercase text-2xl lg:text-6xl tracking-tighter font-bold bg-orange-500 py-2'>Deal Ends January 26th, 2024</h2>
+				<h2 className='uppercase text-2xl lg:text-6xl tracking-tighter font-bold bg-orange-500 py-2'>Deal Ends February 15th, 2025</h2>
 			</div>
 			<div id="demo" className="flex justify-around text-center bg-black py-6 font-garet text-white">
 				<div className="flex flex-col">
